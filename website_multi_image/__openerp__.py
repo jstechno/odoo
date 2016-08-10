812776ca3741cb9d752d5ca90a5ff9700ef921e7
hetal <training@jstechno.com>
2016-08-10 17:18:20 +0530

    Initial Commit

---
+++ website_multi_image/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Product Multi-Image2',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'JSTechno',
+    'depends': ['product', 'sale', 'website_sale'],
+    'data': [
+        'views/product_images.xml',
+        'views/website_product_image_carousel.xml',
+        'views/theme.xml',
+        'views/res_config.xml',
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+}
